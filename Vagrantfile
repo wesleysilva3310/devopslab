94851875778663bf43b0cf6118a046a9bf7ea52d
Wesley Silva <wesley.silva3310@outlook.com>
2024-06-11 18:00:52 -0300

    adding gitlab

--- Vagrantfile
+++ Vagrantfile
@@ -30,7 +30,27 @@ Vagrant.configure("2") do |config|
     node.vm.provision "shell", path: "setup.sh"
   
   end
+# Gitlab
+config.ssh.insert_key = false
 
+config.vm.define "gitlab" do |gitlab|
+
+  gitlab.vm.box               = "ubuntu/focal64"
+  gitlab.vm.hostname          = "gitlab"
+
+  gitlab.vm.network "public_network", ip: "192.168.1.130"
+  
+  gitlab.vm.provider :virtualbox do |gitlabsetup|
+      gitlabsetup.memory = 9000
+      gitlabsetup.cpus = 4
+      end
+
+  gitlab.vm.network "forwarded_port", guest: 80, host: 80
+  gitlab.vm.network "forwarded_port", guest: 443, host: 443
+  gitlab.vm.network "forwarded_port", guest: 2224, host: 2224
+  gitlab.vm.network "forwarded_port", guest: 5050, host: 5050
+  gitlab.vm.provision "shell", path: "setup.sh"
+end
   # Kubernetes Worker Nodes
   NodeCount = 2
 
--- setup.sh
+++ setup.sh
@@ -225,3 +225,38 @@ sudo curl \
     -o /etc/bash_completion.d/docker-compose
 echo "Installation Complete!"
 fi
+
+# Installing gitlab on gitlab server
+if
+        [ "$HOSTNAME" = gitlab ];
+then
+echo "Installing gitlab on $HOSTNAME"
+mkdir /home/vagrant/gitlab
+export GITLAB_HOME=/srv/gitlab
+export GITLAB_HOME=/home/vagrant/gitlab
+cd $GITLAB_HOME
+
+cat > docker-compose.yml << EOF
+version: '3.5'
+services:
+ gitlab:
+  image: 'gitlab/gitlab-ee:latest'
+  restart: always
+  hostname: 'dockerenv-gitlab.wesleylab.com'
+  environment:
+    GITLAB_OMNIBUS_CONFIG: |
+      external_url 'http://dockerenv-gitlab.wesleylab.com'
+      # Add any other gitlab.rb configuration here, each on its own line
+  ports:
+    - '80:80'
+    - '443:443'
+    - '2224:2224'
+  volumes:
+    - '$GITLAB_HOME/config:/etc/gitlab'
+    - '$GITLAB_HOME/logs:/var/log/gitlab'
+    - '$GITLAB_HOME/data:/var/opt/gitlab'
+EOF
+cd gitlab && docker-compose up -d
+# Initial password: docker exec -it gitlab_gitlab_1 cat /etc/gitlab/initial_root_password
+echo "Installation complete!"
+fi
